649da8d5c47423fc35e990f126fbf8b31d0bc338
ConnorEberhart <ceberha4@rockets.utoledo.edu>
2026-08-12 19:16:12 -0400

    finish all functions and create functionability

--- script.js
+++ script.js
@@ -8,7 +8,56 @@ function getComputerChoice() {
 }
 
 function getHumanChoice(){
-    return prompt("Enter \"Rock\", \"Paper\", or \"Scissors\": ");
+    let humanChoice = prompt("Enter \"Rock\", \"Paper\", or \"Scissors\": ");
+    return (humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase());
 }
 
 
+
+
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice){
+        console.log("Tie!")
+    }
+    else if ((humanChoice == "Rock") && (computerChoice == "Paper")) {
+        console.log("You lose! Paper beats Rock!");
+    return false}
+    else if ((humanChoice == "Rock") && (computerChoice == "Scissors")){
+        console.log("You Win! Rock beats Scissors!");
+     return true}
+    else if ((humanChoice == "Paper") && (computerChoice == "Rock")){
+        console.log("You Win! Paper beats Rock!");
+    return true}
+    else if ((humanChoice == "Paper") && (computerChoice == "Scissors")){
+        console.log("You LOSE! Scissors beats Paper!");
+     return false}
+    else if ((humanChoice == "Scissors") && (computerChoice == "Rock")){
+        console.log("You LOSE! Rock beats Scissors!");
+     return false}
+    else if ((humanChoice == "Scissors") && (computerChoice == "Paper")){
+        console.log("You WIN! Scissors beats paper!");
+    return true}
+}
+    
+
+function playGame() {
+   let humanScore = 0;
+   let computerScore = 0;
+    for (let i = 0; i<5; ++i) {
+      let returnVal =  playRound(getHumanChoice(),getComputerChoice());
+        if (returnVal){
+            ++humanScore;
+            console.log(`Score is now, you: ${humanScore} vs computer: ${computerScore}`);
+        }
+        else {
+           ++computerScore;
+           console.log(`Score is now, you: ${humanScore} vs computer: ${computerScore}`);
+        }
+    }
+    console.log(`FINAL SCORE!!!!! , you: ${humanScore} vs computer: ${computerScore}`)
+}
+
+
+playGame();
